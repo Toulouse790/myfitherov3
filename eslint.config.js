93469a98a06b542f88e98515e5353df1beeb8fa9
Toulouse790 <cstophe0@gmail.com>
2025-06-20 11:46:03 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,19 @@
+// eslint.config.js
 import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
 
+/** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
+  {
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {},
+  },
   js.configs.recommended,
+  prettier,
   {
     rules: {
       "no-unused-vars": "warn",
